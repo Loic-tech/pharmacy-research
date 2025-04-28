5cea24519bb9e49bc216cbf8e1acadad1c27fd22
Loic-tech <74209385+Loic-tech@users.noreply.github.com>
2025-04-29 00:32:04 +0200

    New Code to push

---
+++ src/main/java/com/search/pharmacy/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.search.pharmacy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    http.authorizeHttpRequests(
+            authorize ->
+                authorize
+                    .requestMatchers("/subcategories/**", "/medicine/**", "/categories/**")
+                    .permitAll()
+                    .requestMatchers("/orders/**")
+                    .authenticated()
+                    .anyRequest()
+                    .authenticated())
+        .oauth2Login(Customizer.withDefaults());
+    return http.build();
+  }
+}
